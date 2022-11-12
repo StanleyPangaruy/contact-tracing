7741bc29b813497208493b0b77c661de865c3d81
unknown <pangaruystanley@gmail.com>
2022-11-12 13:15:48 +0800

    Implemented a menu

---
+++ contract-tracing.py
@@ -0,0 +1,14 @@
+#Contact Tracing
+
+#create and display a menu of options
+print("""
+=======CONTACT TRACING APP=======
+
+    Menu:
+        1 -> Add an item
+        2 -> Search
+        3 -> Exit (y/n)
+
+=================================""")
+#allow user to choose from the menu
+#perform the selected option
