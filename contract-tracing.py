fc688a3dfec089d430987307191c9b3411708df0
unknown <pangaruystanley@gmail.com>
2022-11-12 15:37:45 +0800

    implemented input validation, else and value error

--- contract-tracing.py
+++ contract-tracing.py
@@ -15,79 +15,48 @@ print("""
 
 contact = {}
 while True:
-    userInput = int(input("What do you want to do? "))
-    if userInput == 1:
-        name = input("Full name: ")
-        age = input("Age: ")
-        address = input("Address: ")
-        phoneNum = input("Phone number: ")
-        contactInfo = {
-            "Name": name,
-            "Age": age,
-            "Address": address,
-            "Phone number": phoneNum
-        }
-        contact[name]=contactInfo
-        print("Saved!")
+    try:
+        userInput = int(input("What do you want to do? "))
         print("=================================")
+        if userInput == 1:
+            print("Add a Contact")
+            name = input("Full name: ")
+            age = input("Age: ")
+            address = input("Address: ")
+            phoneNum = input("Phone number: ")
+            contactInfo = {
+                "Name": name,
+                "Age": age,
+                "Address": address,
+                "Phone number": phoneNum
+            }
+            contact[name]=contactInfo
+            print("Saved!")
+            print("=================================")
 
-    if userInput == 2:
-        fullname = input("Fullname: ")
-        userAge = contact[fullname]["Age"]
-        userAdd = contact[fullname]["Address"]
-        userNum = contact[fullname]["Phone number"]
-        print(f"""Age: {userAge}
+        elif userInput == 2:
+            print("Search Contacts")
+            fullname = input("Fullname: ")
+            userAge = contact[fullname]["Age"]
+            userAdd = contact[fullname]["Address"]
+            userNum = contact[fullname]["Phone number"]
+            print(f"""Age: {userAge}
 Address: {userAdd}
 Phone number: {userNum}
 =================================""")
 
-    # if userInput == 3:
-    #     answer = input("Exit? ")
-    #     if answer == "y":
-    #         break
-
-
-#perform the selected option
-
-# contact = {}
-# while True:
-#     name = input("Full name: ")
-#     age = input("Age: ")
-#     address = input("Address: ")
-#     phoneNum = input("Phone number: ")
-
-#     contactInfo = {
-#         "Name": name,
-#         "Age": age,
-#         "Address": address,
-#         "Phone number": phoneNum
-#     }
-
-#     contact[name]=contactInfo
-#     print(contact)
-
-    # contact[name] = contactInfo
-    # print(contact)
-
-
-
-
-
-
-
-# userInput = int(input("What do you want to do? "))
-# if userInput == 1:
-#     name = input("Full name: ")
-#     age = input("Age: ")
-#     address = input("Address: ")
-#     phoneNum = input("Phone number: ")
-#     contactInfo = {
-#         "Name": name,
-#         "Age": age,
-#         "Address": address,
-#         "Phone number": phoneNum
-#     }
-#     print(contactInfo)  
+        elif userInput == 3:
+            answer = input("Do you want to leave? (y/n) ")
+            if answer == "y":
+                break
+            print("=================================")
+        
+        else:
+            print("Choose from option 1-3")
+            continue
+    except ValueError:
+        print("That is not an option.")
+        print("=================================")
 
 
 
